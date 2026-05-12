1805dc0700383a2bfb91bc668efff87b49b249a8
ios DOMIDIOS <elgamellojr@gmail.com>
2026-05-12 23:57:02 +0200

    lock.m

--- lock.m
+++ lock.m
@@ -1,54 +1,54 @@
 #import <UIKit/UIKit.h>
 
-// Esto obliga a que la función se ejecute apenas la App cargue la dylib
 __attribute__((visibility("default")))
 __attribute__((constructor))
-static void domidios_lock_init() {
-    
-    // Esperamos 3 segundos para que WhatsApp cargue su interfaz completa
+static void init_domidios_lock() {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         
-        // Buscamos la ventana principal de la aplicación
-        UIWindow *window = [UIApplication sharedApplication].keyWindow;
-        if (!window && @available(iOS 13.0, *)) {
-            for (UIWindowScene* scene in [UIApplication sharedApplication].connectedScenes) {
-                if (scene.activationState == UISceneActivationStateForegroundActive) {
-                    window = ((UIWindowScene*)scene).windows.firstObject;
-                    break;
-                }
+        NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
+        NSDate *regDate = [prefs objectForKey:@"domidios_reg_date"];
+        
+        // Comprobar expiración (30 días)
+        if (regDate) {
+            NSTimeInterval elapsed = [[NSDate date] timeIntervalSinceDate:regDate];
+            if (elapsed > 2592000) { // 2,592,000 segundos = 30 días
+                UIViewController *root = [UIApplication sharedApplication].keyWindow.rootViewController;
+                UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"iOS DOMIDIOS" 
+                                            message:@"Tu licencia ha expirado.\nContacta al administrador." 
+                                            preferredStyle:UIAlertControllerStyleAlert];
+                [root presentViewController:alert animated:YES completion:nil];
+                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(4.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{ exit(0); });
+                return;
             }
         }
 
-        UIViewController *rootVC = window.rootViewController;
-        
-        // Si hay una pantalla encima (como la de Watusi), nos ponemos sobre ella
-        while (rootVC.presentedViewController) {
-            rootVC = rootVC.presentedViewController;
-        }
+        // Interfaz de Bloqueo
+        UIWindow *win = [UIApplication sharedApplication].keyWindow;
+        UIViewController *vc = win.rootViewController;
+        while (vc.presentedViewController) vc = vc.presentedViewController;
 
-        if (rootVC) {
-            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"iOS DOMIDIOS"
-                                        message:@"iosDOMIDIOS"
-                                        preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertController *lock = [UIAlertController alertControllerWithTitle:@"SISTEMA DE ACCESO"
+                                    message:@"Introduce tu Key de 30 días"
+                                    preferredStyle:UIAlertControllerStyleAlert];
 
-            [alert addTextFieldWithConfigurationHandler:^(UITextField *textField) {
-                textField.placeholder = @"Contraseña";
-                textField.secureTextEntry = YES;
-            }];
+        [lock addTextFieldWithConfigurationHandler:^(UITextField *tf) {
+            tf.placeholder = @"WTDFGTHGUER";
+            tf.secureTextEntry = YES;
+        }];
 
-            UIAlertAction *entrar = [UIAlertAction actionWithTitle:@"Entrar" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
-                NSString *pass = alert.textFields.firstObject.text;
-                
-                // CAMBIA "1234" POR TU CONTRASEÑA REAL
-                if ([pass isEqualToString:@"iosDOMIDIOS"]) {
-                    NSLog(@"Acceso concedido");
-                } else {
-                    exit(0); // Cierra la app si falla
+        [lock addAction:[UIAlertAction actionWithTitle:@"Activar" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
+            NSString *key = lock.textFields.firstObject.text;
+            
+            if ([key isEqualToString:@"WTDFGTHGUER"]) { // CAMBIA ESTO
+                if (![prefs objectForKey:@"domidios_reg_date"]) {
+                    [prefs setObject:[NSDate date] forKey:@"domidios_reg_date"];
+                    [prefs synchronize];
                 }
-            }];
+            } else {
+                exit(0);
+            }
+        }]];
 
-            [alert addAction:entrar];
-            [rootVC presentViewController:alert animated:YES completion:nil];
-        }
+        [vc presentViewController:lock animated:YES completion:nil];
     });
 }
