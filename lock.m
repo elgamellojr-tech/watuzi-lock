337cf5fd3c58e657afa9cc8f00f1b757b7c85077
ios DOMIDIOS <elgamellojr@gmail.com>
2026-05-12 23:19:48 +0200

    lock.m

--- lock.m
+++ lock.m
@@ -1,40 +1,8 @@
-#import <UIKit/UIKit.h>
-
-// Función para mostrar la alerta de bloqueo
-void showLockAlert(UIViewController *rootVC) {
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"🔐 SISTEMA DOMIDIOS"
-                                                                   message:@"Esta versión de Watusi está protegida.\nIntroduce tu Key de acceso:"
-                                                            preferredStyle:UIAlertControllerStyleAlert];
-
-    [alert addTextFieldWithConfigurationHandler:^(UITextField *textField) {
-        textField.placeholder = @"Key Personal";
-        textField.secureTextEntry = YES;
-        textField.keyboardType = UIKeyboardTypeASCIICapable;
-    }];
-
-    UIAlertAction *validate = [UIAlertAction actionWithTitle:@"VERIFICAR" style:UIAlertActionStyleDefault handler:^(UIAlertAction *action) {
-        UITextField *keyField = alert.textFields.firstObject;
-        
-        // --- AQUÍ CAMBIA LA CONTRASEÑA ---
-        NSString *masterKey = @"iosDOMIDIOS"; 
-        // ---------------------------------
-
-        if ([keyField.text isEqualToString:masterKey]) {
-            NSLog(@"[DOMIDIOS] Acceso autorizado.");
-        } else {
-            // Si la clave es incorrecta, vuelve a mostrar la alerta (bucle de bloqueo)
-            showLockAlert(rootVC);
-        }
-    }];
-
-    [alert addAction:validate];
-    [rootVC presentViewController:alert animated:YES completion:nil];
-}
-
 __attribute__((constructor))
 static void initialize() {
-    // Espera a que la interfaz de la app esté lista
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        
+        // Intentamos obtener la ventana activa de varias formas
         UIWindow *window = nil;
         if (@available(iOS 13.0, *)) {
             for (UIWindowScene* scene in [UIApplication sharedApplication].connectedScenes) {
@@ -43,12 +11,23 @@ static void initialize() {
                     break;
                 }
             }
-        } else {
-            window = [UIApplication sharedApplication].keyWindow;
+        }
+        
+        if (!window) window = [UIApplication sharedApplication].keyWindow;
+
+        UIViewController *rootVC = window.rootViewController;
+        
+        // Si hay un controlador presentado, lo usamos para mostrar nuestra alerta encima
+        while (rootVC.presentedViewController) {
+            rootVC = rootVC.presentedViewController;
         }
 
-        if (window.rootViewController) {
-            showLockAlert(window.rootViewController);
+        if (rootVC) {
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"DOMIDIOS SECURITY"
+                                        message:@"iosDOMIDIOS"
+                                        preferredStyle:UIAlertControllerStyleAlert];
+            // ... resto de tu código de botones ...
+            [rootVC presentViewController:alert animated:YES completion:nil];
         }
     });
 }
