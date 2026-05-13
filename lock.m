809a19e2606b3ed7d297a21fba33cbf926ca4613
ios DOMIDIOS <elgamellojr@gmail.com>
2026-05-13 17:47:51 +0200

    lock.m

--- lock.m
+++ lock.m
@@ -3,6 +3,9 @@
 // --- CONFIGURACIÓN ---
 #define PREF_KEY @"fecha_registro_domidios"
 #define DURACION_DIAS 30
+#define AUTOR_NAME @"iOS DOMIDIOS VIP" 
+
+static void mostrar_contador_vip(UIWindow *window, NSDate *fecha);
 
 __attribute__((visibility("default")))
 __attribute__((constructor))
@@ -23,48 +26,79 @@ static void domidios_premium_init() {
         if (!window) return;
 
         if (fechaActivacion) {
-            // 1. CONTADOR PERMANENTE ROJO (AJUSTADO A Y: 30)
-            UIView *cView = [[UIView alloc] initWithFrame:CGRectMake(0, 30, window.bounds.size.width, 30)];
-            UILabel *timerLabel = [[UILabel alloc] initWithFrame:cView.bounds];
-            timerLabel.textColor = [UIColor redColor];
-            timerLabel.font = [UIFont boldSystemFontOfSize:13];
-            timerLabel.textAlignment = NSTextAlignmentCenter;
-            [cView addSubview:timerLabel];
-            [window addSubview:cView];
-
-            // 2. ACTUALIZACIÓN CADA SEGUNDO
-            [NSTimer scheduledTimerWithTimeInterval:1.0 repeats:YES block:^(NSTimer *timer) {
-                NSTimeInterval r = (DURACION_DIAS * 86400) - [[NSDate date] timeIntervalSinceDate:fechaActivacion];
-                if (r <= 0) exit(0);
-                
-                int d = (int)(r / 86400);
-                int h = (int)((NSInteger)r % 86400) / 3600;
-                int m = (int)((NSInteger)r % 3600) / 60;
-                int s = (int)((NSInteger)r % 60);
-                
-                timerLabel.text = [NSString stringWithFormat:@"VIP: %02dD %02dH %02dM %02dS", d, h, m, s];
-                [window bringSubviewToFront:cView];
-            }];
+            mostrar_contador_vip(window, fechaActivacion);
         } else {
-            // Lógica de Activación
             NSString *deviceID = [[[UIDevice currentDevice] identifierForVendor] UUIDString];
             NSString *shortID = [[deviceID substringToIndex:5] uppercaseString];
             
-            UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"ACTIVACIÓN" 
-                                        message:[NSString stringWithFormat:@"ID: %@", shortID] 
+            UIAlertController *alert = [UIAlertController alertControllerWithTitle:[NSString stringWithFormat:@"\n🔐 %@", AUTOR_NAME] 
+                                        message:[NSString stringWithFormat:@"Device Signature: %@\n\nPlease enter your license key.", shortID] 
                                         preferredStyle:UIAlertControllerStyleAlert];
-            [alert addTextFieldWithConfigurationHandler:nil];
-            [alert addAction:[UIAlertAction actionWithTitle:@"VERIFICAR" style:UIAlertActionStyleDefault handler:^(UIAlertAction *a) {
-                if ([alert.textFields.firstObject.text isEqualToString:[NSString stringWithFormat:@"VIP-%@-7", shortID]]) {
-                    [prefs setObject:[NSDate date] forKey:PREF_KEY];
+
+            [alert addTextFieldWithConfigurationHandler:^(UITextField *textField) {
+                textField.placeholder = @"entra tu key aqui";
+                textField.textAlignment = NSTextAlignmentCenter;
+                textField.secureTextEntry = YES; // Modo privado
+                textField.keyboardAppearance = UIKeyboardAppearanceDark;
+                textField.font = [UIFont fontWithName:@"Helvetica-Bold" size:15];
+            }];
+
+            UIAlertAction *authAction = [UIAlertAction actionWithTitle:@"ACEPTAR" style:UIAlertActionStyleDefault handler:^(UIAlertAction *a) {
+                NSString *input = alert.textFields.firstObject.text;
+                NSString *masterKey = [NSString stringWithFormat:@"WRT%@27", shortID];
+
+                if ([input isEqualToString:masterKey]) {
+                    NSDate *ahora = [NSDate date];
+                    [prefs setObject:ahora forKey:PREF_KEY];
                     [prefs synchronize];
+                    
+                    // --- CONFIRMACIÓN DE ÉXITO ---
+                    UIAlertController *success = [UIAlertController alertControllerWithTitle:@"ÉXITO" 
+                                                 message:@"Autorización completada correctamente." 
+                                                 preferredStyle:UIAlertControllerStyleAlert];
+                    
+                    UIViewController *root = window.rootViewController;
+                    while(root.presentedViewController) root = root.presentedViewController;
+                    
+                    [root presentViewController:success animated:YES completion:^{
+                        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                            [success dismissViewControllerAnimated:YES completion:^{
+                                mostrar_contador_vip(window, ahora);
+                            }];
+                        });
+                    }];
+                } else {
                     exit(0);
-                } else { exit(0); }
-            }]];
+                }
+            }];
+
+            [alert addAction:authAction];
             
             UIViewController *root = window.rootViewController;
             while(root.presentedViewController) root = root.presentedViewController;
             [root presentViewController:alert animated:YES completion:nil];
         }
     });
+}
+
+// --- FUNCIÓN DEL CONTADOR (POSICIÓN Y: 30) ---
+static void mostrar_contador_vip(UIWindow *window, NSDate *fecha) {
+    // Se mantiene en y: 30 como pediste
+    UIView *cView = [[UIView alloc] initWithFrame:CGRectMake(0, 30, window.bounds.size.width, 30)];
+    UILabel *timerLabel = [[UILabel alloc] initWithFrame:cView.bounds];
+    timerLabel.textColor = [UIColor redColor]; // Color Rojo
+    timerLabel.font = [UIFont boldSystemFontOfSize:13];
+    timerLabel.textAlignment = NSTextAlignmentCenter;
+    [cView addSubview:timerLabel];
+    [window addSubview:cView];
+
+    [NSTimer scheduledTimerWithTimeInterval:1.0 repeats:YES block:^(NSTimer *timer) {
+        NSTimeInterval r = (DURACION_DIAS * 86400) - [[NSDate date] timeIntervalSinceDate:fecha];
+        if (r <= 0) exit(0);
+        
+        int d = (int)(r / 86400), h = (int)((NSInteger)r % 86400) / 3600, m = (int)((NSInteger)r % 3600) / 60, s = (int)((NSInteger)r % 60);
+        
+        timerLabel.text = [NSString stringWithFormat:@"%02dD %02dH %02dM %02dS", d, h, m, s];
+        [window bringSubviewToFront:cView];
+    }];
 }
