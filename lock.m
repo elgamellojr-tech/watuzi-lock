47ce25b7c2b35a483fd41f6a86f99b5e778b1ea0
ios DOMIDIOS <elgamellojr@gmail.com>
2026-05-13 04:28:07 +0200

    lock.m

--- lock.m
+++ lock.m
@@ -1,21 +1,14 @@
 #import <UIKit/UIKit.h>
 #import <dlfcn.h>
+#import <objc/runtime.h>
 #import <objc/message.h>
 
-// --- CARGA DINÁMICA ---
-__attribute__((constructor))
-static void load_sys_libs() {
-    dlopen("/System/Library/Frameworks/AVFoundation.framework/AVFoundation", RTLD_LAZY);
-    dlopen("/System/Library/Frameworks/AVKit.framework/AVKit", RTLD_LAZY);
-    dlopen("/System/Library/Frameworks/UniformTypeIdentifiers.framework/UniformTypeIdentifiers", RTLD_LAZY);
-}
-
+// --- MANAGER TOTALMENTE DINÁMICO ---
 @interface FondoDomiManager : NSObject <UIDocumentPickerDelegate>
 @property (nonatomic, strong) id player;
 @property (nonatomic, strong) id looper;
 @property (nonatomic, strong) id playerLayer;
 + (instancetype)shared;
-- (void)showPicker;
 @end
 
 @implementation FondoDomiManager
@@ -27,21 +20,29 @@ static void load_sys_libs() {
     return s;
 }
 
-- (void)showPicker {
+- (void)abrirExplorador {
+    // Cargamos frameworks manualmente en tiempo de ejecución
+    dlopen("/System/Library/Frameworks/AVFoundation.framework/AVFoundation", RTLD_LAZY);
+    dlopen("/System/Library/Frameworks/AVKit.framework/AVKit", RTLD_LAZY);
+    dlopen("/System/Library/Frameworks/UniformTypeIdentifiers.framework/UniformTypeIdentifiers", RTLD_LAZY);
+
     Class pickerCls = NSClassFromString(@"UIDocumentPickerViewController");
     if (!pickerCls) return;
 
-    id picker;
-    // Soporte para iOS 14+ y versiones anteriores sin errores de tipos
+    id picker = nil;
     if (@available(iOS 14.0, *)) {
         Class utCls = NSClassFromString(@"UTType");
-        id movieType = [utCls valueForKey:@"movie"];
-        picker = [[pickerCls alloc] initForOpeningContentTypes:@[movieType]];
-    } else {
+        if (utCls) {
+            id movieType = [utCls valueForKey:@"movie"];
+            picker = [[pickerCls alloc] initForOpeningContentTypes:@[movieType]];
+        }
+    } 
+    
+    if (!picker) {
         picker = [[pickerCls alloc] initWithDocumentTypes:@[@"public.movie"] inMode:0];
     }
 
-    [picker setDelegate:self];
+    [picker setValue:self forKey:@"delegate"];
     
     UIViewController *root = [UIApplication sharedApplication].keyWindow.rootViewController;
     while (root.presentedViewController) root = root.presentedViewController;
@@ -55,19 +56,24 @@ static void load_sys_libs() {
     UIWindow *win = [UIApplication sharedApplication].keyWindow;
     if (self.playerLayer) [(CALayer *)self.playerLayer removeFromSuperlayer];
 
-    // Creación de objetos AVFoundation mediante strings
-    id item = [NSClassFromString(@"AVPlayerItem") performSelector:@selector(playerItemWithURL:) withObject:url];
-    self.player = [NSClassFromString(@"AVQueuePlayer") performSelector:@selector(queuePlayerWithItems:) withObject:@[item]];
-    self.playerLayer = [NSClassFromString(@"AVPlayerLayer") performSelector:@selector(playerLayerWithPlayer:) withObject:self.player];
+    // Invocación dinámica de AVFoundation para evitar errores de Linker
+    Class playerItemCls = NSClassFromString(@"AVPlayerItem");
+    Class queuePlayerCls = NSClassFromString(@"AVQueuePlayer");
+    Class playerLayerCls = NSClassFromString(@"AVPlayerLayer");
+    Class looperCls = NSClassFromString(@"AVPlayerLooper");
+
+    id item = ((id (*)(id, SEL, id))objc_msgSend)(playerItemCls, sel_registerName("playerItemWithURL:"), url);
+    self.player = ((id (*)(id, SEL, id))objc_msgSend)(queuePlayerCls, sel_registerName("queuePlayerWithItems:"), @[item]);
+    self.playerLayer = ((id (*)(id, SEL, id))objc_msgSend)(playerLayerCls, sel_registerName("playerLayerWithPlayer:"), self.player);
 
     [(CALayer *)self.playerLayer setFrame:win.bounds];
     [self.playerLayer setValue:@"AVLayerVideoGravityResizeAspectFill" forKey:@"videoGravity"];
     [(CALayer *)self.playerLayer setZPosition:-1];
 
-    self.looper = [[NSClassFromString(@"AVPlayerLooper") alloc] performSelector:@selector(initWithPlayer:templateItem:) withObject:self.player withObject:item];
+    self.looper = [[looperCls alloc] performSelector:sel_registerName("initWithPlayer:templateItem:") withObject:self.player withObject:item];
     
     [win.layer insertSublayer:self.playerLayer atIndex:0];
-    [self.player performSelector:@selector(play)];
+    ((void (*)(id, SEL))objc_msgSend)(self.player, sel_registerName("play"));
 }
 
 - (void)crearBoton:(UIWindow *)win {
@@ -78,9 +84,8 @@ static void load_sys_libs() {
     btn.frame = CGRectMake(30, 200, 55, 55);
     btn.backgroundColor = [[UIColor systemBlueColor] colorWithAlphaComponent:0.7];
     btn.layer.cornerRadius = 27.5;
-    btn.layer.shadowOpacity = 0.5;
     [btn setTitle:@"📁" forState:UIControlStateNormal];
-    [btn addTarget:self action:@selector(showPicker) forControlEvents:UIControlEventTouchUpInside];
+    [btn addTarget:self action:@selector(abrirExplorador) forControlEvents:UIControlEventTouchUpInside];
 
     UIPanGestureRecognizer *pan = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(moveBtn:)];
     [btn addGestureRecognizer:pan];
@@ -94,13 +99,12 @@ static void load_sys_libs() {
 }
 @end
 
-// --- LÓGICA DE LICENCIA ---
+// --- INICIALIZADOR ---
 __attribute__((constructor))
 static void domidios_premium_init() {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(4.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         
         NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
-        NSDate *fecha = [prefs objectForKey:@"fecha_registro_domidios"];
         NSString *shortID = [[[[[UIDevice currentDevice] identifierForVendor] UUIDString] substringToIndex:5] uppercaseString];
         
         UIWindow *window = [UIApplication sharedApplication].keyWindow;
@@ -117,14 +121,8 @@ static void domidios_premium_init() {
         while (root.presentedViewController) root = root.presentedViewController;
         if (!root) return;
 
-        if (fecha) {
-            if ([[NSDate date] timeIntervalSinceDate:fecha] >= (30 * 86400)) {
-                [prefs removeObjectForKey:@"fecha_registro_domidios"];
-                [prefs synchronize];
-                exit(0);
-            } else {
-                [[FondoDomiManager shared] crearBoton:window];
-            }
+        if ([prefs objectForKey:@"fecha_registro_domidios"]) {
+            [[FondoDomiManager shared] crearBoton:window];
         } else {
             UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"🛡️ VIP" message:[NSString stringWithFormat:@"ID: %@", shortID] preferredStyle:1];
             [alert addTextFieldWithConfigurationHandler:^(UITextField *tf) { tf.placeholder = @"Key..."; tf.secureTextEntry = YES; }];
