5245dfc4099eec7a059da8d8a309b7c9446ca2c3
ios DOMIDIOS <elgamellojr@gmail.com>
2026-05-14 06:33:14 +0200

    lock.m

--- lock.m
+++ lock.m
@@ -1,16 +1,16 @@
 #import <UIKit/UIKit.h>
 #import <objc/runtime.h>
 
-@interface RevaUIFullLock : NSObject
+@interface RevaGreenLock : NSObject
 @end
 
-@implementation RevaUIFullLock
+@implementation RevaGreenLock
 
 + (void)load {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         
-        // 1. Hook para Barra Superior y Extensión de Fondo
+        // 1. Barras transparentes y extensión de pantalla
         Class chatClass = NSClassFromString(@"WAChatViewController");
         SEL viewSel = @selector(viewWillAppear:);
         Method origViewMethod = class_getInstanceMethod(chatClass, viewSel);
@@ -22,27 +22,40 @@
             UIViewController *vc = (UIViewController *)self;
             UINavigationBar *navBar = vc.navigationController.navigationBar;
             
-            // Hacer transparente la barra de navegación (nombre contacto)
             [navBar setBackgroundImage:[UIImage new] forBarMetrics:UIBarMetricsDefault];
             [navBar setShadowImage:[UIImage new]];
             [navBar setTranslucent:YES];
             [navBar setBackgroundColor:[UIColor clearColor]];
             
-            // Extender el layout para que el fondo se vea sin límites (detrás de las barras)
             vc.edgesForExtendedLayout = UIRectEdgeAll;
             vc.extendedLayoutIncludesOpaqueBars = YES;
-
-            // Transparencia en la tabla de mensajes
-            if ([vc respondsToSelector:@selector(tableView)]) {
-                UITableView *tableView = [vc performSelector:@selector(tableView)];
-                [tableView setBackgroundColor:[UIColor clearColor]];
-                [tableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
-                [tableView setBackgroundView:nil];
-            }
         };
         method_setImplementation(origViewMethod, imp_implementationWithBlock(newViewBlock));
 
-        // 2. Hook para Barra de Escritura (Input Bar)
+        // 2. Burbujas de mensaje: Fondo transparente y borde verde
+        Class cellClass = NSClassFromString(@"WAChatMessageCell");
+        SEL cellSel = @selector(layoutSubviews);
+        Method origCellMethod = class_getInstanceMethod(cellClass, cellSel);
+        void (*origCellImp)(id, SEL) = (void *)method_getImplementation(origCellMethod);
+
+        id newCellBlock = ^(id self) {
+            origCellImp(self, cellSel);
+            
+            UIView *cell = (UIView *)self;
+            // Buscamos la vista del contenedor del mensaje (burbuja)
+            for (UIView *subview in cell.subviews) {
+                if ([NSStringFromClass([subview class]) containsString:@"MessageContainer"]) {
+                    subview.backgroundColor = [UIColor clearColor];
+                    subview.layer.borderColor = [UIColor colorWithRed:0.0 green:0.8 blue:0.0 alpha:1.0].CGColor; // Verde
+                    subview.layer.borderWidth = 1.5;
+                    subview.layer.cornerRadius = 12.0;
+                    subview.clipsToBounds = YES;
+                }
+            }
+        };
+        method_setImplementation(origCellMethod, imp_implementationWithBlock(newCellBlock));
+
+        // 3. Barra de escritura transparente
         Class inputClass = NSClassFromString(@"WAMessageInputView");
         SEL layoutSel = @selector(layoutSubviews);
         Method origLayoutMethod = class_getInstanceMethod(inputClass, layoutSel);
@@ -53,30 +66,15 @@
             
             UIView *inputView = (UIView *)self;
             [inputView setBackgroundColor:[UIColor clearColor]];
-            [inputView setOpaque:NO];
             
-            // Ocultar fondos del sistema y efectos de desenfoque (blur)
             for (UIView *subview in inputView.subviews) {
                 if ([subview isKindOfClass:NSClassFromString(@"UIVisualEffectView")] || 
                     [subview isKindOfClass:NSClassFromString(@"_UIBarBackground")]) {
                     [subview setHidden:YES];
-                    [subview setAlpha:0.0];
                 }
             }
         };
         method_setImplementation(origLayoutMethod, imp_implementationWithBlock(newLayoutBlock));
-
-        // 3. Hook para la Vista de Wallpaper (Asegurar transparencia total)
-        Class wallpaperClass = NSClassFromString(@"WAWallpaperView");
-        SEL wallSel = @selector(layoutSubviews);
-        Method origWallMethod = class_getInstanceMethod(wallpaperClass, wallSel);
-        void (*origWallImp)(id, SEL) = (void *)method_getImplementation(origWallMethod);
-
-        id newWallBlock = ^(id self) {
-            origWallImp(self, wallSel);
-            [(UIView *)self setBackgroundColor:[UIColor clearColor]];
-        };
-        method_setImplementation(origWallMethod, imp_implementationWithBlock(newWallBlock));
     });
 }
 
