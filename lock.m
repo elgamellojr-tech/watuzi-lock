8a9adc20dc6e35765d240ed67da1ba6686c23bf6
ios DOMIDIOS <elgamellojr@gmail.com>
2026-05-14 06:53:51 +0200

    lock.m

--- lock.m
+++ lock.m
@@ -1,105 +1,121 @@
+/*
+  -------------------------------------------------------------------------
+  MODELO DE PATCH.PLIST (ESTILO FLEX 3 JAILBREAK)
+  -------------------------------------------------------------------------
+  <dict>
+    <key>Name</key> <string>WhatsApp Reva UI Transparent</string>
+    <key>Author</key> <string>iOS DOMIDIOS</string>
+    <key>Units</key>
+    <array>
+      <dict>
+        <key>MethodObjc</key> <dict><key>ClassName</key><string>WAChatViewController</string><key>Selector</key><string>viewWillAppear:</string></dict>
+        <key>Action</key> <string>Force Transparent Navigation Bar</string>
+      </dict>
+      <dict>
+        <key>MethodObjc</key> <dict><key>ClassName</key><string>WAMessageChatTableViewCell</string><key>Selector</key><string>layoutSubviews</string></dict>
+        <key>Action</key> <string>Set Bubble Background 0.0 & Border Green</string>
+      </dict>
+    </array>
+  </dict>
+  -------------------------------------------------------------------------
+*/
+
 #import <UIKit/UIKit.h>
 #import <objc/runtime.h>
 
-@interface RevaUIUltra : NSObject
+@interface Flex3Engine : NSObject
 @end
 
-@implementation RevaUIUltra
+@implementation Flex3Engine
 
 + (void)load {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         
-        // --- 1. HOOK PARA ELIMINAR EL FONDO DE LAS BURBUJAS ---
-        // Hookeamos el método que configura el fondo de la burbuja
-        Class bubbleViewClass = NSClassFromString(@"WBBubbleView") ?: NSClassFromString(@"WABubbleView");
-        if (bubbleViewClass) {
-            SEL setBgSel = @selector(setBackgroundColor:);
-            Method origSetBg = class_getInstanceMethod(bubbleViewClass, setBgSel);
-            id bgBlock = ^(id self, UIColor *color) {
-                // Forzamos que cualquier intento de poner color sea transparente
-                ((void (*)(id, SEL, UIColor *))method_getImplementation(origSetBg))(self, setBgSel, [UIColor clearColor]);
+        // --- PARCHE FLEX: TRANSPARENCIA DE BARRA SUPERIOR ---
+        Class chatClass = NSClassFromString(@"WAChatViewController");
+        if (chatClass) {
+            SEL viewSel = @selector(viewWillAppear:);
+            Method origViewMethod = class_getInstanceMethod(chatClass, viewSel);
+            void (*origViewImp)(id, SEL, BOOL) = (void *)method_getImplementation(origViewMethod);
+            
+            id newViewBlock = ^(id self, BOOL animated) {
+                origViewImp(self, viewSel, animated);
+                
+                UIViewController *vc = (UIViewController *)self;
+                UINavigationBar *navBar = vc.navigationController.navigationBar;
+                
+                // Aplicando valores de parche Flex
+                [navBar setBackgroundImage:[UIImage new] forBarMetrics:UIBarMetricsDefault];
+                [navBar setShadowImage:[UIImage new]];
+                [navBar setTranslucent:YES];
+                [navBar setBackgroundColor:[UIColor clearColor]];
+                
+                vc.edgesForExtendedLayout = UIRectEdgeAll;
+                vc.extendedLayoutIncludesOpaqueBars = YES;
             };
-            method_setImplementation(origSetBg, imp_implementationWithBlock(bgBlock));
+            method_setImplementation(origViewMethod, imp_implementationWithBlock(newViewBlock));
         }
 
-        // --- 2. APLICAR BORDE VERDE Y TRANSPARENCIA EN LA CELDA ---
+        // --- PARCHE FLEX: BURBUJAS REVA (BORDE VERDE + TRANSPARENCIA) ---
         Class cellClass = NSClassFromString(@"WAMessageChatTableViewCell");
-        SEL layoutSel = @selector(layoutSubviews);
-        Method origLayout = class_getInstanceMethod(cellClass, layoutSel);
-        void (*origLayoutImp)(id, SEL) = (void *)method_getImplementation(origLayout);
+        if (cellClass) {
+            SEL layoutSel = @selector(layoutSubviews);
+            Method origLayoutMethod = class_getInstanceMethod(cellClass, layoutSel);
+            void (*origLayoutImp)(id, SEL) = (void *)method_getImplementation(origLayoutMethod);
 
-        id layoutBlock = ^(id self) {
-            origLayoutImp(self, layoutSel);
-            
-            UIView *cell = (UIView *)self;
-            cell.backgroundColor = [UIColor clearColor];
-
-            // Buscamos la burbuja dentro de la celda
-            for (UIView *subview in cell.subviews) {
-                if ([NSStringFromClass([subview class]) containsString:@"Bubble"] || 
-                    [NSStringFromClass([subview class]) containsString:@"Container"]) {
-                    
-                    subview.backgroundColor = [UIColor clearColor];
-                    subview.layer.backgroundColor = [UIColor clearColor].CGColor;
-                    
-                    // Estilo Reva: Borde verde neón y esquinas redondeadas
-                    subview.layer.borderColor = [UIColor colorWithRed:0.0 green:1.0 blue:0.5 alpha:1.0].CGColor;
-                    subview.layer.borderWidth = 1.5;
-                    subview.layer.cornerRadius = 18.0;
-                    subview.layer.masksToBounds = YES;
-
-                    // Ocultar cualquier imagen (la burbuja original de WA)
-                    for (UIView *inner in subview.subviews) {
-                        if ([inner isKindOfClass:[UIImageView class]]) {
-                            inner.hidden = YES;
+            id newLayoutBlock = ^(id self) {
+                origLayoutImp(self, layoutSel);
+                
+                UIView *cell = (UIView *)self;
+                for (UIView *subview in cell.subviews) {
+                    // Detectando el contenedor de la burbuja
+                    if ([NSStringFromClass([subview class]) containsString:@"Bubble"] || 
+                        [NSStringFromClass([subview class]) containsString:@"Container"]) {
+                        
+                        // Forzando transparencia (Flex Unit: Background Alpha 0)
+                        subview.backgroundColor = [UIColor clearColor];
+                        subview.layer.backgroundColor = [UIColor clearColor].CGColor;
+                        
+                        // Forzando Borde Verde (Flex Unit: BorderColor Green)
+                        subview.layer.borderColor = [UIColor colorWithRed:0.0 green:1.0 blue:0.5 alpha:1.0].CGColor;
+                        subview.layer.borderWidth = 1.5;
+                        subview.layer.cornerRadius = 16.0;
+                        subview.layer.masksToBounds = YES;
+                        
+                        // Ocultando la imagen original de la burbuja de WhatsApp
+                        for (UIView *inner in subview.subviews) {
+                            if ([inner isKindOfClass:[UIImageView class]]) {
+                                inner.hidden = YES;
+                            }
                         }
                     }
                 }
-            }
-        };
-        method_setImplementation(origLayout, imp_implementationWithBlock(layoutBlock));
+            };
+            method_setImplementation(origLayoutMethod, imp_implementationWithBlock(newLayoutBlock));
+        }
 
-        // --- 3. BARRAS TOTALMENTE TRANSPARENTES (NAV & INPUT) ---
-        Class chatClass = NSClassFromString(@"WAChatViewController");
-        SEL viewSel = @selector(viewWillAppear:);
-        Method origView = class_getInstanceMethod(chatClass, viewSel);
-        void (*origViewImp)(id, SEL, BOOL) = (void *)method_getImplementation(origView);
-
-        id viewBlock = ^(id self, BOOL animated) {
-            origViewImp(self, viewSel, animated);
-            UIViewController *vc = (UIViewController *)self;
-            
-            // Transparencia arriba
-            UINavigationBar *nb = vc.navigationController.navigationBar;
-            [nb setBackgroundImage:[UIImage new] forBarMetrics:UIBarMetricsDefault];
-            [nb setShadowImage:[UIImage new]];
-            [nb setTranslucent:YES];
-            [nb setBackgroundColor:[UIColor clearColor]];
-            
-            vc.edgesForExtendedLayout = UIRectEdgeAll;
-            vc.extendedLayoutIncludesOpaqueBars = YES;
-        };
-        method_setImplementation(origView, imp_implementationWithBlock(viewBlock));
-
-        // Transparencia abajo (Barra de escribir)
+        // --- PARCHE FLEX: BARRA DE ENTRADA TRANSPARENTE ---
         Class inputClass = NSClassFromString(@"WAMessageInputView");
-        SEL inLayoutSel = @selector(layoutSubviews);
-        Method origInLayout = class_getInstanceMethod(inputClass, inLayoutSel);
-        void (*origInImp)(id, SEL) = (void *)method_getImplementation(origInLayout);
+        if (inputClass) {
+            SEL inSel = @selector(layoutSubviews);
+            Method origInMethod = class_getInstanceMethod(inputClass, inSel);
+            void (*origInImp)(id, SEL) = (void *)method_getImplementation(origInMethod);
 
-        id inBlock = ^(id self) {
-            origInImp(self, inLayoutSel);
-            UIView *inV = (UIView *)self;
-            inV.backgroundColor = [UIColor clearColor];
-            for (UIView *s in inV.subviews) {
-                if ([s isKindOfClass:NSClassFromString(@"UIVisualEffectView")] || 
-                    [s isKindOfClass:NSClassFromString(@"_UIBarBackground")]) {
-                    s.hidden = YES;
+            id newInBlock = ^(id self) {
+                origInImp(self, inSel);
+                UIView *inputV = (UIView *)self;
+                inputV.backgroundColor = [UIColor clearColor];
+                for (UIView *sub in inputV.subviews) {
+                    if ([sub isKindOfClass:NSClassFromString(@"UIVisualEffectView")] || 
+                        [sub isKindOfClass:NSClassFromString(@"_UIBarBackground")]) {
+                        sub.hidden = YES;
+                    }
                 }
-            }
-        };
-        method_setImplementation(origInLayout, imp_implementationWithBlock(inBlock));
+            };
+            method_setImplementation(origInMethod, imp_implementationWithBlock(newInBlock));
+        }
     });
 }
+
 @end
