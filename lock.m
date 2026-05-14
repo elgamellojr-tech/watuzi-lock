eeae7269d68c4e5cc4ef768c4f6f55b8bd4126d9
ios DOMIDIOS <elgamellojr@gmail.com>
2026-05-14 05:57:21 +0200

    lock.m

--- lock.m
+++ lock.m
@@ -1,60 +1,66 @@
 #import <UIKit/UIKit.h>
 #import <objc/runtime.h>
 
-// Importamos las funciones de Cydia Substrate
-#include <substrate.h>
+@interface TransparentHook : NSObject
+@end
 
-// --- Hook para WAChatViewController ---
-static void (*orig_WAChatViewController_viewWillAppear)(id, SEL, BOOL);
-void replaced_WAChatViewController_viewWillAppear(id self, SEL _cmd, BOOL animated) {
-    orig_WAChatViewController_viewWillAppear(self, _cmd, animated);
-    
-    // Obtenemos la Navigation Bar
-    UINavigationController *navController = [self performSelector:@selector(navigationController)];
-    UINavigationBar *navBar = navController.navigationBar;
-    
-    // Transparencia total
-    [navBar setBackgroundImage:[UIImage new] forBarMetrics:UIBarMetricsDefault];
-    [navBar setShadowImage:[UIImage new]];
-    [navBar setTranslucent:YES];
-    [navBar setBackgroundColor:[UIColor clearColor]];
-    
-    // Extender el layout
-    [(UIViewController *)self setEdgesForExtendedLayout:UIRectEdgeAll];
-    [(UIViewController *)self setExtendedLayoutIncludesOpaqueBars:YES];
+@implementation TransparentHook
+
++ (void)load {
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        // Hook para la barra superior (WAChatViewController)
+        Class chatClass = NSClassFromString(@"WAChatViewController");
+        SEL viewSel = @selector(viewWillAppear:);
+        Method origViewMethod = class_getInstanceMethod(chatClass, viewSel);
+        
+        void (*origViewImp)(id, SEL, BOOL) = (void *)method_getImplementation(origViewMethod);
+        
+        id newViewBlock = ^(id self, BOOL animated) {
+            origViewImp(self, viewSel, animated);
+            
+            UIViewController *vc = (UIViewController *)self;
+            UINavigationBar *navBar = vc.navigationController.navigationBar;
+            
+            // Hacer transparente la barra de arriba
+            [navBar setBackgroundImage:[UIImage new] forBarMetrics:UIBarMetricsDefault];
+            [navBar setShadowImage:[UIImage new]];
+            [navBar setTranslucent:YES];
+            [navBar setBackgroundColor:[UIColor clearColor]];
+            
+            // Expandir el fondo del chat detrás de las barras
+            vc.edgesForExtendedLayout = UIRectEdgeAll;
+            vc.extendedLayoutIncludesOpaqueBars = YES;
+        };
+        
+        method_setImplementation(origViewMethod, imp_implementationWithBlock(newViewBlock));
+
+        // Hook para la barra de escritura (WAMessageInputView)
+        Class inputClass = NSClassFromString(@"WAMessageInputView");
+        SEL layoutSel = @selector(layoutSubviews);
+        Method origLayoutMethod = class_getInstanceMethod(inputClass, layoutSel);
+        
+        void (*origLayoutImp)(id, SEL) = (void *)method_getImplementation(origLayoutMethod);
+        
+        id newLayoutBlock = ^(id self) {
+            origLayoutImp(self, layoutSel);
+            
+            UIView *inputView = (UIView *)self;
+            [inputView setBackgroundColor:[UIColor clearColor]];
+            [inputView setOpaque:NO];
+            
+            // Quitar el blur y fondos grises de la barra de escribir
+            for (UIView *subview in inputView.subviews) {
+                if ([subview isKindOfClass:NSClassFromString(@"UIVisualEffectView")] || 
+                    [subview isKindOfClass:NSClassFromString(@"_UIBarBackground")]) {
+                    [subview setHidden:YES];
+                    [subview setAlpha:0.0];
+                }
+            }
+        };
+        
+        method_setImplementation(origLayoutMethod, imp_implementationWithBlock(newLayoutBlock));
+    });
 }
 
-// --- Hook para WAMessageInputView ---
-static void (*orig_WAMessageInputView_layoutSubviews)(id, SEL);
-void replaced_WAMessageInputView_layoutSubviews(id self, SEL _cmd) {
-    orig_WAMessageInputView_layoutSubviews(self, _cmd);
-    
-    UIView *inputView = (UIView *)self;
-    [inputView setBackgroundColor:[UIColor clearColor]];
-    [inputView setOpaque:NO];
-
-    // Ocultar capas de fondo/blur
-    for (UIView *subview in inputView.subviews) {
-        if ([subview isKindOfClass:NSClassFromString(@"UIVisualEffectView")] || 
-            [subview isKindOfClass:NSClassFromString(@"_UIBarBackground")]) {
-            [subview setHidden:YES];
-            [subview setAlpha:0.0];
-        }
-    }
-}
-
-// --- Constructor (Se ejecuta al cargar el dylib) ---
-__attribute__((constructor))
-static void initialize() {
-    // Hooking WAChatViewController
-    MSHookMessageEx(NSClassFromString(@"WAChatViewController"), 
-                    @selector(viewWillAppear:), 
-                    (IMP)replaced_WAChatViewController_viewWillAppear, 
-                    (IMP *)&orig_WAChatViewController_viewWillAppear);
-    
-    // Hooking WAMessageInputView
-    MSHookMessageEx(NSClassFromString(@"WAMessageInputView"), 
-                    @selector(layoutSubviews), 
-                    (IMP)replaced_WAMessageInputView_layoutSubviews, 
-                    (IMP *)&orig_WAMessageInputView_layoutSubviews);
-}
+@end
