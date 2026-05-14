cf64ad30555c07c380d1be818e8c0fe6a3cd814b
ios DOMIDIOS <elgamellojr@gmail.com>
2026-05-14 06:45:54 +0200

    lock.m

--- lock.m
+++ lock.m
@@ -1,105 +1,105 @@
 #import <UIKit/UIKit.h>
 #import <objc/runtime.h>
 
-@interface RevaUIFinalV2 : NSObject
+@interface RevaUIUltra : NSObject
 @end
 
-@implementation RevaUIFinalV2
+@implementation RevaUIUltra
 
 + (void)load {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         
-        // 1. FORZAR TRANSPARENCIA EN BARRAS (SUPERIOR E INFERIOR)
-        Class chatClass = NSClassFromString(@"WAChatViewController");
-        SEL viewSel = @selector(viewWillAppear:);
-        Method origViewMethod = class_getInstanceMethod(chatClass, viewSel);
-        void (*origViewImp)(id, SEL, BOOL) = (void *)method_getImplementation(origViewMethod);
-        
-        id newViewBlock = ^(id self, BOOL animated) {
-            origViewImp(self, viewSel, animated);
-            UIViewController *vc = (UIViewController *)self;
-            
-            UINavigationBar *navBar = vc.navigationController.navigationBar;
-            [navBar setBackgroundImage:[UIImage new] forBarMetrics:UIBarMetricsDefault];
-            [navBar setShadowImage:[UIImage new]];
-            [navBar setTranslucent:YES];
-            [navBar setBackgroundColor:[UIColor clearColor]];
-            
-            vc.edgesForExtendedLayout = UIRectEdgeAll;
-            vc.extendedLayoutIncludesOpaqueBars = YES;
+        // --- 1. HOOK PARA ELIMINAR EL FONDO DE LAS BURBUJAS ---
+        // Hookeamos el método que configura el fondo de la burbuja
+        Class bubbleViewClass = NSClassFromString(@"WBBubbleView") ?: NSClassFromString(@"WABubbleView");
+        if (bubbleViewClass) {
+            SEL setBgSel = @selector(setBackgroundColor:);
+            Method origSetBg = class_getInstanceMethod(bubbleViewClass, setBgSel);
+            id bgBlock = ^(id self, UIColor *color) {
+                // Forzamos que cualquier intento de poner color sea transparente
+                ((void (*)(id, SEL, UIColor *))method_getImplementation(origSetBg))(self, setBgSel, [UIColor clearColor]);
+            };
+            method_setImplementation(origSetBg, imp_implementationWithBlock(bgBlock));
+        }
 
-            if ([vc respondsToSelector:@selector(tableView)]) {
-                UITableView *tv = [vc performSelector:@selector(tableView)];
-                tv.backgroundColor = [UIColor clearColor];
-                tv.backgroundView = nil;
-            }
-        };
-        method_setImplementation(origViewMethod, imp_implementationWithBlock(newViewBlock));
-
-        // 2. ESTILO DE BURBUJAS REVA (BORDE VERDE + TRANSPARENCIA REAL)
-        // Probamos con la clase base de la celda de mensaje
+        // --- 2. APLICAR BORDE VERDE Y TRANSPARENCIA EN LA CELDA ---
         Class cellClass = NSClassFromString(@"WAMessageChatTableViewCell");
         SEL layoutSel = @selector(layoutSubviews);
-        Method origCellMethod = class_getInstanceMethod(cellClass, layoutSel);
-        void (*origCellImp)(id, SEL) = (void *)method_getImplementation(origCellMethod);
+        Method origLayout = class_getInstanceMethod(cellClass, layoutSel);
+        void (*origLayoutImp)(id, SEL) = (void *)method_getImplementation(origLayout);
 
-        id newCellBlock = ^(id self) {
-            origCellImp(self, layoutSel);
-            UIView *cell = (UIView *)self;
+        id layoutBlock = ^(id self) {
+            origLayoutImp(self, layoutSel);
             
-            // Buscamos profundamente en todas las subvistas
+            UIView *cell = (UIView *)self;
+            cell.backgroundColor = [UIColor clearColor];
+
+            // Buscamos la burbuja dentro de la celda
             for (UIView *subview in cell.subviews) {
-                // Buscamos cualquier vista que contenga la burbuja (Bubble o Container)
-                if ([NSStringFromClass([subview class])兴奋String:@"Bubble"] || 
+                if ([NSStringFromClass([subview class]) containsString:@"Bubble"] || 
                     [NSStringFromClass([subview class]) containsString:@"Container"]) {
                     
                     subview.backgroundColor = [UIColor clearColor];
                     subview.layer.backgroundColor = [UIColor clearColor].CGColor;
                     
-                    // Aplicar el borde verde característico de Reva
-                    subview.layer.borderColor = [UIColor colorWithRed:0.0 green:0.9 blue:0.4 alpha:1.0].CGColor;
-                    subview.layer.borderWidth = 1.4;
-                    subview.layer.cornerRadius = 16.0;
+                    // Estilo Reva: Borde verde neón y esquinas redondeadas
+                    subview.layer.borderColor = [UIColor colorWithRed:0.0 green:1.0 blue:0.5 alpha:1.0].CGColor;
+                    subview.layer.borderWidth = 1.5;
+                    subview.layer.cornerRadius = 18.0;
                     subview.layer.masksToBounds = YES;
-                    
-                    // ELIMINAR EL DIBUJO ORIGINAL DE WHATSAPP
-                    // Esto oculta las imágenes de las burbujas que tapan el fondo
-                    for (UIView *innerView in subview.subviews) {
-                        if ([innerView isKindOfClass:[UIImageView class]]) {
-                            innerView.hidden = YES;
-                        }
-                        // Si la burbuja es una vista de dibujo especial
-                        if ([NSStringFromClass([innerView class]) containsString:@"Shape"]) {
-                            innerView.hidden = YES;
+
+                    // Ocultar cualquier imagen (la burbuja original de WA)
+                    for (UIView *inner in subview.subviews) {
+                        if ([inner isKindOfClass:[UIImageView class]]) {
+                            inner.hidden = YES;
                         }
                     }
                 }
             }
         };
-        method_setImplementation(origCellMethod, imp_implementationWithBlock(newCellBlock));
+        method_setImplementation(origLayout, imp_implementationWithBlock(layoutBlock));
 
-        // 3. BARRA DE ESCRITURA TOTALMENTE INVISIBLE
+        // --- 3. BARRAS TOTALMENTE TRANSPARENTES (NAV & INPUT) ---
+        Class chatClass = NSClassFromString(@"WAChatViewController");
+        SEL viewSel = @selector(viewWillAppear:);
+        Method origView = class_getInstanceMethod(chatClass, viewSel);
+        void (*origViewImp)(id, SEL, BOOL) = (void *)method_getImplementation(origView);
+
+        id viewBlock = ^(id self, BOOL animated) {
+            origViewImp(self, viewSel, animated);
+            UIViewController *vc = (UIViewController *)self;
+            
+            // Transparencia arriba
+            UINavigationBar *nb = vc.navigationController.navigationBar;
+            [nb setBackgroundImage:[UIImage new] forBarMetrics:UIBarMetricsDefault];
+            [nb setShadowImage:[UIImage new]];
+            [nb setTranslucent:YES];
+            [nb setBackgroundColor:[UIColor clearColor]];
+            
+            vc.edgesForExtendedLayout = UIRectEdgeAll;
+            vc.extendedLayoutIncludesOpaqueBars = YES;
+        };
+        method_setImplementation(origView, imp_implementationWithBlock(viewBlock));
+
+        // Transparencia abajo (Barra de escribir)
         Class inputClass = NSClassFromString(@"WAMessageInputView");
         SEL inLayoutSel = @selector(layoutSubviews);
-        Method origInMethod = class_getInstanceMethod(inputClass, inLayoutSel);
-        void (*origInImp)(id, SEL) = (void *)method_getImplementation(origInMethod);
+        Method origInLayout = class_getInstanceMethod(inputClass, inLayoutSel);
+        void (*origInImp)(id, SEL) = (void *)method_getImplementation(origInLayout);
 
-        id newInBlock = ^(id self) {
+        id inBlock = ^(id self) {
             origInImp(self, inLayoutSel);
-            UIView *view = (UIView *)self;
-            view.backgroundColor = [UIColor clearColor];
-            
-            for (UIView *sub in view.subviews) {
-                // Removemos el fondo gris y el blur
-                if ([sub isKindOfClass:NSClassFromString(@"UIVisualEffectView")] || 
-                    [sub isKindOfClass:NSClassFromString(@"_UIBarBackground")]) {
-                    sub.hidden = YES;
+            UIView *inV = (UIView *)self;
+            inV.backgroundColor = [UIColor clearColor];
+            for (UIView *s in inV.subviews) {
+                if ([s isKindOfClass:NSClassFromString(@"UIVisualEffectView")] || 
+                    [s isKindOfClass:NSClassFromString(@"_UIBarBackground")]) {
+                    s.hidden = YES;
                 }
             }
         };
-        method_setImplementation(origInMethod, imp_implementationWithBlock(newInBlock));
+        method_setImplementation(origInLayout, imp_implementationWithBlock(inBlock));
     });
 }
-
 @end
