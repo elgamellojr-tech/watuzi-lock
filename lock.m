05558376426f7a29038a0e1e5deac5b05df9a73e
ios DOMIDIOS <elgamellojr@gmail.com>
2026-05-14 05:46:04 +0200

    lock.m

--- lock.m
+++ lock.m
@@ -1,122 +1,56 @@
 #import <UIKit/UIKit.h>
 
-// --- CONFIGURACIÓN ---
-#define PREF_KEY @"fecha_registro_domidios"
-#define DURACION_DIAS 30
-#define AUTOR_NAME @"iOS DOMIDIOS VIP" 
+// --- TRANSPARENCIA EN LA BARRA SUPERIOR (NAV BAR) ---
+// Hookeamos el controlador del chat para modificar la barra de navegación
+%hook WAChatViewController
 
-static void mostrar_contador_vip(UIWindow *window, NSDate *fecha);
-static void verificar_y_bloquear(void);
-
-__attribute__((visibility("default")))
-__attribute__((constructor))
-static void domidios_premium_init() {
-    // Observador para detectar cuando la app vuelve de segundo plano
-    [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationDidBecomeActiveNotification 
-                                                      object:nil 
-                                                       queue:[NSOperationQueue mainQueue] 
-                                                  usingBlock:^(NSNotification * _Nonnull note) {
-        verificar_y_bloquear();
-    }];
-
-    // Ejecución inicial
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        verificar_y_bloquear();
-    });
+- (void)viewWillAppear:(BOOL)animated {
+    %orig;
+    
+    // Accedemos a la barra de navegación
+    UINavigationBar *navBar = self.navigationController.navigationBar;
+    
+    // Eliminamos el fondo y la línea divisoria
+    [navBar setBackgroundImage:[UIImage new] forBarMetrics:UIBarMetricsDefault];
+    [navBar setShadowImage:[UIImage new]];
+    [navBar setTranslucent:YES];
+    [navBar setBackgroundColor:[UIColor clearColor]];
+    
+    // Forzamos a que el contenido del chat suba hasta arriba de la pantalla
+    self.edgesForExtendedLayout = UIRectEdgeAll;
+    self.extendedLayoutIncludesOpaqueBars = YES;
 }
 
-static void verificar_y_bloquear() {
-    NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
-    NSDate *fechaActivacion = [prefs objectForKey:PREF_KEY];
+%end
+
+// --- TRANSPARENCIA EN LA BARRA DE ESCRITURA (INPUT BAR) ---
+// Hookeamos la vista de entrada de mensajes
+%hook WAMessageInputView
+
+- (void)layoutSubviews {
+    %orig;
     
-    UIWindow *window = [UIApplication sharedApplication].keyWindow;
-    if (!window && @available(iOS 13.0, *)) {
-        for (UIWindowScene* scene in [UIApplication sharedApplication].connectedScenes) {
-            if (scene.activationState == UISceneActivationStateForegroundActive) {
-                window = scene.windows.firstObject; break;
-            }
+    // Hacemos el fondo de la vista transparente
+    [self setBackgroundColor:[UIColor clearColor]];
+    [self setOpaque:NO];
+
+    // Buscamos y ocultamos el fondo difuminado (UIVisualEffectView) que usa WhatsApp
+    for (UIView *subview in self.subviews) {
+        if ([subview isKindOfClass:NSClassFromString(@"UIVisualEffectView")] || 
+            [subview isKindOfClass:NSClassFromString(@"_UIBarBackground")]) {
+            [subview setHidden:YES];
+            [subview setAlpha:0.0];
         }
-    }
-    
-    if (!window) return;
-
-    if (fechaActivacion) {
-        mostrar_contador_vip(window, fechaActivacion);
-    } else {
-        // Si ya hay una alerta presentada, no duplicarla, pero si no hay nada, BLOQUEAR
-        UIViewController *root = window.rootViewController;
-        while(root.presentedViewController) {
-            if ([root.presentedViewController isKindOfClass:[UIAlertController class]]) return;
-            root = root.presentedViewController;
-        }
-
-        NSString *deviceID = [[[UIDevice currentDevice] identifierForVendor] UUIDString];
-        NSString *shortID = [[deviceID substringToIndex:5] uppercaseString];
-        
-        UIAlertController *alert = [UIAlertController alertControllerWithTitle:[NSString stringWithFormat:@"\n🔐 %@", AUTOR_NAME] 
-                                    message:[NSString stringWithFormat:@"iD: %@\n\nPlease enter your license key.", shortID] 
-                                    preferredStyle:UIAlertControllerStyleAlert];
-
-        [alert addTextFieldWithConfigurationHandler:^(UITextField *textField) {
-            textField.placeholder = @"entra tu key aqui";
-            textField.textAlignment = NSTextAlignmentCenter;
-            textField.secureTextEntry = YES;
-            textField.keyboardAppearance = UIKeyboardAppearanceDark;
-        }];
-
-        UIAlertAction *authAction = [UIAlertAction actionWithTitle:@"ACEPTAR" style:UIAlertActionStyleDefault handler:^(UIAlertAction *a) {
-            NSString *input = alert.textFields.firstObject.text;
-            // Tu lógica de llave: WRT + ID + 27
-            NSString *masterKey = [NSString stringWithFormat:@"WRT%@27", shortID];
-
-            if ([input isEqualToString:masterKey]) {
-                NSDate *ahora = [NSDate date];
-                [prefs setObject:ahora forKey:PREF_KEY];
-                [prefs synchronize];
-                
-                UIAlertController *success = [UIAlertController alertControllerWithTitle:@"ÉXITO" 
-                                             message:@"Autorización completada correctamente." 
-                                             preferredStyle:UIAlertControllerStyleAlert];
-                
-                [window.rootViewController presentViewController:success animated:YES completion:^{
-                    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-                        [success dismissViewControllerAnimated:YES completion:^{
-                            mostrar_contador_vip(window, ahora);
-                        }];
-                    });
-                }];
-            } else {
-                exit(0);
-            }
-        }];
-
-        [alert addAction:authAction];
-        [root presentViewController:alert animated:YES completion:nil];
     }
 }
 
-static void mostrar_contador_vip(UIWindow *window, NSDate *fecha) {
-    // Evitar duplicar el contador si ya existe
-    if ([window viewWithTag:999]) return;
+%end
 
-    UIView *cView = [[UIView alloc] initWithFrame:CGRectMake(0, 30, window.bounds.size.width, 30)];
-    cView.tag = 999; // Tag para identificarlo
-    UILabel *timerLabel = [[UILabel alloc] initWithFrame:cView.bounds];
-    timerLabel.textColor = [UIColor redColor];
-    timerLabel.font = [UIFont boldSystemFontOfSize:13];
-    timerLabel.textAlignment = NSTextAlignmentCenter;
-    [cView addSubview:timerLabel];
-    [window addSubview:cView];
-
-    [NSTimer scheduledTimerWithTimeInterval:1.0 repeats:YES block:^(NSTimer *timer) {
-        NSTimeInterval r = (DURACION_DIAS * 86400) - [[NSDate date] timeIntervalSinceDate:fecha];
-        if (r <= 0) {
-            [[NSUserDefaults standardUserDefaults] removeObjectForKey:PREF_KEY];
-            exit(0);
-        }
-        
-        int d = (int)(r / 86400), h = (int)((NSInteger)r % 86400) / 3600, m = (int)((NSInteger)r % 3600) / 60, s = (int)((NSInteger)r % 60);
-        timerLabel.text = [NSString stringWithFormat:@"%02dD %02dH %02dM %02dS", d, h, m, s];
-        [window bringSubviewToFront:cView];
-    }];
+// --- COMPATIBILIDAD CON EL CONTENEDOR DE LA BARRA ---
+%hook WAMessageInputContainerView
+- (void)layoutSubviews {
+    %orig;
+    [self setBackgroundColor:[UIColor clearColor]];
+    [self setOpaque:NO];
 }
+%end
