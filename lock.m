e2ad7f60fa42936624bb8cc07583df850d86ea49
ios DOMIDIOS <elgamellojr@gmail.com>
2026-05-14 22:47:10 +0200

    lock.m

--- lock.m
+++ lock.m
@@ -1,6 +1,8 @@
 #import <UIKit/UIKit.h>
 #import <QuartzCore/QuartzCore.h>
+#import <objc/runtime.h>
 
+// --- VISTA DINÁMICA ---
 @interface DynamicBackgroundView : UIView
 @end
 
@@ -9,10 +11,10 @@
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-        self.userInteractionEnabled = NO; // Para que no interfiera con los toques
+        self.userInteractionEnabled = NO;
+        self.tag = 888;
         [self startInfiniteAnimation];
         
-        // SUSCRIPCIÓN CRÍTICA: Reiniciar cuando la app vuelve a primer plano
         [[NSNotificationCenter defaultCenter] addObserver:self 
                                                  selector:@selector(startInfiniteAnimation) 
                                                      name:UIApplicationWillEnterForegroundNotification 
@@ -22,49 +24,64 @@
 }
 
 - (void)startInfiniteAnimation {
-    // 1. Limpieza total de animaciones colgadas
     [self.layer removeAllAnimations];
 
-    // 2. Definir la animación (ejemplo de pulso o brillo)
     CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"opacity"];
     animation.fromValue = @(1.0);
     animation.toValue = @(0.4);
     animation.duration = 2.0; 
     animation.autoreverses = YES;
     animation.repeatCount = HUGE_VALF;
-    
-    // 3. LA CLAVE: Estas dos líneas evitan que iOS borre la animación al pausar
     animation.removedOnCompletion = NO;
     animation.fillMode = kCAFillModeForwards;
 
-    // 4. Aplicar
     [self.layer addAnimation:animation forKey:@"persistentLoop"];
 }
 
 - (void)dealloc {
     [[NSNotificationCenter defaultCenter] removeObserver:self];
+    // Solución al warning de dealloc
+    #if !__has_feature(objc_arc)
+    [super dealloc];
+    #endif
 }
 
 @end
 
-// --- HOOK DE INTEGRACIÓN ---
+// --- LÓGICA DE INYECCIÓN (SWIZZLING) ---
 
-%hook WatusiLockViewController
-
-- (void)viewDidAppear:(BOOL)animated {
-    %orig;
-    
-    // Buscar si ya existe para no duplicar
-    DynamicBackgroundView *existingBg = [self.view viewWithTag:888];
-    
-    if (!existingBg) {
-        DynamicBackgroundView *dynamicBg = [[DynamicBackgroundView alloc] initWithFrame:self.view.bounds];
-        dynamicBg.tag = 888;
-        [self.view insertSubview:dynamicBg atIndex:0];
-    } else {
-        // Forzar reinicio si ya existe pero está parado
-        [existingBg startInfiniteAnimation];
-    }
+void swizzleMethod(Class class, SEL originalSelector, SEL swizzledSelector) {
+    Method originalMethod = class_getInstanceMethod(class, originalSelector);
+    Method swizzledMethod = class_getInstanceMethod(class, swizzledSelector);
+    method_exchangeImplementations(originalMethod, swizzledMethod);
 }
 
-%end
+@interface UIViewController (WatusiFix)
+- (void)new_viewDidAppear:(BOOL)animated;
+@end
+
+@implementation UIViewController (WatusiFix)
+- (void)new_viewDidAppear:(BOOL)animated {
+    [self new_viewDidAppear:animated]; // Llama al original
+
+    // Verificamos si estamos en la pantalla de bloqueo de Watusi
+    if ([NSStringFromClass([self class]) isEqualToString:@"WatusiLockViewController"]) {
+        DynamicBackgroundView *existingBg = [self.view viewWithTag:888];
+        if (!existingBg) {
+            DynamicBackgroundView *dynamicBg = [[DynamicBackgroundView alloc] initWithFrame:self.view.bounds];
+            [self.view insertSubview:dynamicBg atIndex:0];
+        } else {
+            [existingBg startInfiniteAnimation];
+        }
+    }
+}
+@end
+
+// Esto se ejecuta automáticamente al cargar la dylib
+__attribute__((constructor))
+static void initialize() {
+    Class targetClass = NSClassFromString(@"WatusiLockViewController");
+    if (targetClass) {
+        swizzleMethod(targetClass, @selector(viewDidAppear:), @selector(new_viewDidAppear:));
+    }
+}
