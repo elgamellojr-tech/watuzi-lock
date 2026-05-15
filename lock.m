0a03997e9098d2407dc990e099ff38db78dca5bf
ios DOMIDIOS <elgamellojr@gmail.com>
2026-05-15 06:54:26 +0200

    lock.m

--- lock.m
+++ lock.m
@@ -2,13 +2,9 @@
 #import <CoreGraphics/CoreGraphics.h>
 #import <objc/runtime.h>
 
-// --- VISTA PERSONALIZADA ---
 @interface MyCustomBar : UIView
-// Usamos assign para evitar el error de sintaxis en compilación manual
 @property (nonatomic, assign) UITabBarController *parentController;
 @property (nonatomic, retain) UIView *selectionIndicator;
-@property (nonatomic, retain) UIButton *chatButton;
-@property (nonatomic, retain) UIButton *profileButton;
 @end
 
 @implementation MyCustomBar
@@ -17,123 +13,117 @@
     self = [super initWithFrame:frame];
     if (self) {
         _parentController = parent;
-        
-        self.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.5];
+        self.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.6];
         self.layer.cornerRadius = frame.size.height / 2;
         self.layer.masksToBounds = YES;
-        self.userInteractionEnabled = YES;
+        self.userInteractionEnabled = YES; // IMPORTANTE
 
+        // Blur de fondo
         UIVisualEffectView *blur = [[UIVisualEffectView alloc] initWithEffect:[UIBlurEffect effectWithStyle:UIBlurEffectStyleDark]];
         blur.frame = self.bounds;
         blur.userInteractionEnabled = NO;
         [self addSubview:blur];
 
-        _selectionIndicator = [[UIView alloc] initWithFrame:CGRectMake(5, 5, (frame.size.width/2) - 10, frame.size.height - 10)];
-        _selectionIndicator.backgroundColor = [UIColor colorWithWhite:1.0 alpha:0.15];
+        // Indicador Gris
+        _selectionIndicator = [[UIView alloc] initWithFrame:CGRectMake(5, 5, (frame.size.width/2)-10, frame.size.height-10)];
+        _selectionIndicator.backgroundColor = [UIColor colorWithWhite:1.0 alpha:0.2];
         _selectionIndicator.layer.cornerRadius = _selectionIndicator.frame.size.height / 2;
         _selectionIndicator.userInteractionEnabled = NO;
         [self addSubview:_selectionIndicator];
 
-        _chatButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        _chatButton.frame = CGRectMake(0, 0, frame.size.width/2, frame.size.height);
-        [_chatButton setTitle:@"Chats" forState:UIControlStateNormal];
-        [_chatButton addTarget:self action:@selector(goToChats) forControlEvents:UIControlEventTouchUpInside];
-        [self addSubview:_chatButton];
+        // BOTÓN IZQUIERDO (CHATS)
+        UIButton *btnChat = [UIButton buttonWithType:UIButtonTypeCustom];
+        btnChat.frame = CGRectMake(0, 0, frame.size.width/2, frame.size.height);
+        [btnChat setTitle:@"Chats" forState:UIControlStateNormal];
+        [btnChat addTarget:self action:@selector(tapChat) forControlEvents:UIControlEventTouchUpInside];
+        [self addSubview:btnChat];
 
-        _profileButton = [UIButton buttonWithType:UIButtonTypeCustom];
-        _profileButton.frame = CGRectMake(frame.size.width/2, 0, frame.size.width/2, frame.size.height);
-        [_profileButton setTitle:@"Perfil" forState:UIControlStateNormal];
+        // BOTÓN DERECHO (PERFIL/ESTADOS)
+        UIButton *btnPerfil = [UIButton buttonWithType:UIButtonTypeCustom];
+        btnPerfil.frame = CGRectMake(frame.size.width/2, 0, frame.size.width/2, frame.size.height);
+        [btnPerfil setTitle:@"Perfil" forState:UIControlStateNormal];
         
+        // Menú Nativo de iOS
         if (@available(iOS 14.0, *)) {
-            UIAction *verEstado = [UIAction actionWithTitle:@"Ver Estados" image:nil identifier:nil handler:^(__kindof UIAction *action) {
-                [self goToUpdates];
+            UIAction *act1 = [UIAction actionWithTitle:@"Ver Estados" image:nil identifier:nil handler:^(__kindof UIAction *action) {
+                [self tapEstado];
             }];
-            UIAction *ajustes = [UIAction actionWithTitle:@"Ajustes" image:nil identifier:nil handler:^(__kindof UIAction *action) {
-                [self goToSettings];
+            UIAction *act2 = [UIAction actionWithTitle:@"Ajustes" image:nil identifier:nil handler:^(__kindof UIAction *action) {
+                [self tapAjustes];
             }];
-            _profileButton.menu = [UIMenu menuWithTitle:@"" children:@[verEstado, ajustes]];
-            _profileButton.showsMenuAsPrimaryAction = YES;
+            btnPerfil.menu = [UIMenu menuWithTitle:@"" children:@[act1, act2]];
+            btnPerfil.showsMenuAsPrimaryAction = YES;
         } else {
-            [_profileButton addTarget:self action:@selector(goToSettings) forControlEvents:UIControlEventTouchUpInside];
+            [btnPerfil addTarget:self action:@selector(tapAjustes) forControlEvents:UIControlEventTouchUpInside];
         }
-        [self addSubview:_profileButton];
+        [self addSubview:btnPerfil];
     }
     return self;
 }
 
-- (void)goToChats {
+// Lógica de cambio de pestañas
+- (void)tapChat {
     [UIView animateWithDuration:0.2 animations:^{
         self.selectionIndicator.frame = CGRectMake(5, 5, (self.frame.size.width/2)-10, self.frame.size.height-10);
     }];
-    if (self.parentController) {
-        [self.parentController setSelectedIndex:3];
-    }
+    if (self.parentController) [self.parentController setSelectedIndex:3]; // Chats suele ser 3
 }
 
-- (void)goToUpdates {
-    if (self.parentController) {
-        [self.parentController setSelectedIndex:0];
-    }
+- (void)tapEstado {
+    if (self.parentController) [self.parentController setSelectedIndex:0]; // Estados es 0
 }
 
-- (void)goToSettings {
+- (void)tapAjustes {
     [UIView animateWithDuration:0.2 animations:^{
         self.selectionIndicator.frame = CGRectMake((self.frame.size.width/2)+5, 5, (self.frame.size.width/2)-10, self.frame.size.height-10);
     }];
-    if (self.parentController) {
-        [self.parentController setSelectedIndex:4];
-    }
+    if (self.parentController) [self.parentController setSelectedIndex:4]; // Configuración es 4
 }
+
+// Esto asegura que la barra detecte los toques aunque haya cosas "debajo"
+- (UIView *)hitTest:(CGPoint)point withEvent:(UIEvent *)event {
+    UIView *hitView = [super hitTest:point withEvent:event];
+    if (hitView == self) return nil; 
+    return hitView;
+}
+
 @end
 
 // --- HOOK ---
-static void (*orig_viewDidLayout)(UIViewController *, SEL);
+static void (*orig_layout)(UIViewController *, SEL);
 
-void hooked_viewDidLayout(UIViewController *self, SEL _cmd) {
-    orig_viewDidLayout(self, _cmd);
+void hooked_layout(UIViewController *self, SEL _cmd) {
+    orig_layout(self, _cmd);
 
     if (![NSStringFromClass([self class]) isEqualToString:@"WATabBarController"]) return;
 
-    UITabBarController *tabC = (UITabBarController *)self;
-    tabC.tabBar.hidden = YES;
+    UITabBarController *tab = (UITabBarController *)self;
+    tab.tabBar.hidden = YES;
+    tab.tabBar.userInteractionEnabled = NO;
 
-    UIWindow *keyWindow = nil;
-    if (@available(iOS 13.0, *)) {
-        for (UIWindowScene* scene in [UIApplication sharedApplication].connectedScenes) {
-            if (scene.activationState == UISceneActivationStateForegroundActive) {
-                for (UIWindow* window in scene.windows) {
-                    if (window.isKeyWindow) {
-                        keyWindow = window;
-                        break;
-                    }
-                }
-            }
-        }
-    } else {
-        keyWindow = [UIApplication sharedApplication].keyWindow;
+    UIWindow *win = self.view.window;
+    if (!win) return;
+
+    MyCustomBar *bar = (MyCustomBar *)[win viewWithTag:888];
+    if (!bar) {
+        bar = [[MyCustomBar alloc] initWithFrame:CGRectMake((win.frame.size.width-280)/2, win.frame.size.height-100, 280, 65) parent:tab];
+        bar.tag = 888;
+        [win addSubview:bar];
     }
-
-    if (!keyWindow) return;
-
-    MyCustomBar *myBar = (MyCustomBar *)[keyWindow viewWithTag:888];
-    if (!myBar) {
-        myBar = [[MyCustomBar alloc] initWithFrame:CGRectMake((keyWindow.frame.size.width - 280)/2, keyWindow.frame.size.height - 100, 280, 65) parent:tabC];
-        myBar.tag = 888;
-        [keyWindow addSubview:myBar];
-    }
-
-    [keyWindow bringSubviewToFront:myBar];
-
-    BOOL isInMain = (tabC.presentedViewController == nil && tabC.navigationController.viewControllers.count <= 1);
-    myBar.hidden = !isInMain;
+    
+    [win bringSubviewToFront:bar]; // Forzar al frente
+    
+    // Ocultar si entramos a un chat
+    BOOL isRoot = (tab.presentedViewController == nil && tab.navigationController.viewControllers.count <= 1);
+    bar.hidden = !isRoot;
 }
 
 __attribute__((constructor))
 static void init() {
-    Class targetClass = objc_getClass("WATabBarController");
-    if (targetClass) {
-        Method m = class_getInstanceMethod(targetClass, @selector(viewDidLayoutSubviews));
-        orig_viewDidLayout = (void *)method_getImplementation(m);
-        method_setImplementation(m, (IMP)hooked_viewDidLayout);
+    Class c = objc_getClass("WATabBarController");
+    if (c) {
+        Method m = class_getInstanceMethod(c, @selector(viewDidLayoutSubviews));
+        orig_layout = (void *)method_getImplementation(m);
+        method_setImplementation(m, (IMP)hooked_layout);
     }
 }
