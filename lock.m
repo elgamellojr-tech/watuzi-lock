db27d1eb58d83b80e5c91b96aab8255d6eaf17a2
ios DOMIDIOS <elgamellojr@gmail.com>
2026-05-15 06:29:37 +0200

    lock.m

--- lock.m
+++ lock.m
@@ -16,8 +16,10 @@
         self.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.4];
         self.layer.cornerRadius = frame.size.height / 2;
         self.layer.masksToBounds = YES;
+        self.userInteractionEnabled = YES;
 
-        _blurView = [[UIVisualEffectView alloc] initWithEffect:[UIBlurEffect effectWithStyle:UIBlurEffectStyleDark]];
+        UIBlurEffect *blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleDark];
+        _blurView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
         _blurView.frame = self.bounds;
         [self addSubview:_blurView];
 
@@ -28,67 +30,84 @@
 
         _chatButton = [UIButton buttonWithType:UIButtonTypeCustom];
         _chatButton.frame = CGRectMake(0, 0, frame.size.width/2, frame.size.height);
-        [_chatButton setTitle:@"チャット" forState:UIControlStateNormal];
+        [_chatButton setTitle:@"Chats" forState:UIControlStateNormal];
         [_chatButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-        _chatButton.titleLabel.font = [UIFont boldSystemFontOfSize:11];
+        _chatButton.titleLabel.font = [UIFont boldSystemFontOfSize:13];
         [self addSubview:_chatButton];
 
         _profileButton = [UIButton buttonWithType:UIButtonTypeCustom];
         _profileButton.frame = CGRectMake(frame.size.width/2, 0, frame.size.width/2, frame.size.height);
-        [_profileButton setTitle:@"自分" forState:UIControlStateNormal];
+        [_profileButton setTitle:@"Perfil" forState:UIControlStateNormal];
         [_profileButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-        _profileButton.titleLabel.font = [UIFont boldSystemFontOfSize:11];
+        _profileButton.titleLabel.font = [UIFont boldSystemFontOfSize:13];
 
-        // Protección para evitar errores en versiones viejas de iOS
         if (@available(iOS 14.0, *)) {
-            UIAction *opcion1 = [UIAction actionWithTitle:@"Ajustes" image:[UIImage systemImageNamed:@"gear"] identifier:nil handler:^(__kindof UIAction *action) {}];
-            UIAction *opcion2 = [UIAction actionWithTitle:@"Estados" image:[UIImage systemImageNamed:@"circle.dashed"] identifier:nil handler:^(__kindof UIAction *action) {}];
-            
-            _profileButton.menu = [UIMenu menuWithTitle:@"Opciones" children:@[opcion1, opcion2]];
+            UIAction *ajustes = [UIAction actionWithTitle:@"Ajustes" image:nil identifier:nil handler:^(__kindof UIAction *action) {}];
+            _profileButton.menu = [UIMenu menuWithTitle:@"" children:@[ajustes]];
             _profileButton.showsMenuAsPrimaryAction = YES;
         }
-
         [self addSubview:_profileButton];
     }
     return self;
 }
 @end
 
-// --- HOOKING MANUAL (Objective-C Puro) ---
-static void (*orig_viewDidAppear)(UIViewController *, SEL, BOOL);
+// --- HOOKING ---
+static void (*orig_viewDidLayoutSubviews)(UIViewController *, SEL);
 
-void hooked_viewDidAppear(UIViewController *self, SEL _cmd, BOOL animated) {
-    orig_viewDidAppear(self, _cmd, animated);
+void hooked_viewDidLayoutSubviews(UIViewController *self, SEL _cmd) {
+    orig_viewDidLayoutSubviews(self, _cmd);
 
-    if ([self.view viewWithTag:999]) return;
+    NSString *className = NSStringFromClass([self class]);
+    UIWindow *keyWindow = [UIApplication sharedApplication].keyWindow;
 
-    // Solo aplicar a la TabBar de WhatsApp
-    if ([NSStringFromClass([self class]) isEqualToString:@"WATabBarController"]) {
-        CGFloat barWidth = 260;
-        CGFloat barHeight = 65;
-        MyCustomBar *customBar = [[MyCustomBar alloc] initWithFrame:CGRectMake(
-            (self.view.frame.size.width - barWidth) / 2,
-            self.view.frame.size.height - 100,
-            barWidth,
-            barHeight
-        )];
-        customBar.tag = 999;
-        [self.view addSubview:customBar];
+    if ([className isEqualToString:@"WATabBarController"]) {
+        UITabBarController *tabController = (UITabBarController *)self;
         
-        // Ocultar la original
-        if ([self respondsToSelector:@selector(tabBar)]) {
-            [(UITabBarController *)self tabBar].hidden = YES;
+        // 1. Ocultar la barra original agresivamente
+        tabController.tabBar.hidden = YES;
+        tabController.tabBar.alpha = 0;
+        tabController.tabBar.frame = CGRectZero; 
+
+        // 2. Manejar nuestra barra personalizada
+        UIView *myBar = [keyWindow viewWithTag:888];
+        if (!myBar) {
+            CGFloat width = 280;
+            CGFloat height = 65;
+            myBar = [[MyCustomBar alloc] initWithFrame:CGRectMake(
+                (keyWindow.frame.size.width - width) / 2,
+                keyWindow.frame.size.height - 95,
+                width,
+                height
+            )];
+            myBar.tag = 888;
+            [keyWindow addSubview:myBar];
         }
+        
+        // Siempre traer al frente para que no la tape nada
+        [keyWindow bringSubviewToFront:myBar];
+        myBar.hidden = NO;
+    } 
+    // Ocultar si entramos a un chat (identificando la clase del controlador de mensajes)
+    else if ([className containsString:@"WAChat"] || [className containsString:@"Message"]) {
+        UIView *myBar = [keyWindow viewWithTag:888];
+        if (myBar) myBar.hidden = YES;
     }
 }
 
-// Constructor para inyectar el código al cargar la dylib
 __attribute__((constructor))
 static void init() {
+    // Usamos viewDidLayoutSubviews para que se ejecute cada vez que la UI se refresque
     Class targetClass = objc_getClass("WATabBarController");
     if (targetClass) {
-        Method origMethod = class_getInstanceMethod(targetClass, @selector(viewDidAppear:));
-        orig_viewDidAppear = (void *)method_getImplementation(origMethod);
-        method_setImplementation(origMethod, (IMP)hooked_viewDidAppear);
+        Method m = class_getInstanceMethod(targetClass, @selector(viewDidLayoutSubviews));
+        orig_viewDidLayoutSubviews = (void *)method_getImplementation(m);
+        method_setImplementation(m, (IMP)hooked_viewDidLayoutSubviews);
     }
+    
+    // Hook adicional para asegurar que se oculte al entrar a cualquier otra pantalla
+    Class baseClass = [UIViewController class];
+    Method m2 = class_getInstanceMethod(baseClass, @selector(viewDidLayoutSubviews));
+    orig_viewDidLayoutSubviews = (void *)method_getImplementation(m2);
+    method_setImplementation(m2, (IMP)hooked_viewDidLayoutSubviews);
 }
