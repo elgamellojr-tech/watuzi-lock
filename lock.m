efc167fc0f27137bc64a42fa3b3a6bf0ecea15e5
ios DOMIDIOS <elgamellojr@gmail.com>
2026-05-15 06:45:24 +0200

    lock.m

--- lock.m
+++ lock.m
@@ -4,7 +4,7 @@
 
 // --- VISTA PERSONALIZADA ---
 @interface MyCustomBar : UIView
-@property (nonatomic, strong) UITabBarController *parentController;
+@property (nonatomic, weak) UITabBarController *parentController;
 @property (nonatomic, strong) UIView *selectionIndicator;
 @property (nonatomic, strong) UIButton *chatButton;
 @property (nonatomic, strong) UIButton *profileButton;
@@ -16,20 +16,24 @@
     self = [super initWithFrame:frame];
     if (self) {
         _parentController = parent;
+        
+        // Configuración de la vista principal
         self.backgroundColor = [[UIColor blackColor] colorWithAlphaComponent:0.5];
         self.layer.cornerRadius = frame.size.height / 2;
         self.layer.masksToBounds = YES;
-        self.userInteractionEnabled = YES;
+        self.userInteractionEnabled = YES; // CRITICO: Habilitar interacción
 
         // Efecto Blur
         UIVisualEffectView *blur = [[UIVisualEffectView alloc] initWithEffect:[UIBlurEffect effectWithStyle:UIBlurEffectStyleDark]];
         blur.frame = self.bounds;
+        blur.userInteractionEnabled = NO; // El blur no debe capturar toques
         [self addSubview:blur];
 
         // Indicador de selección
         _selectionIndicator = [[UIView alloc] initWithFrame:CGRectMake(5, 5, (frame.size.width/2) - 10, frame.size.height - 10)];
         _selectionIndicator.backgroundColor = [UIColor colorWithWhite:1.0 alpha:0.15];
         _selectionIndicator.layer.cornerRadius = _selectionIndicator.frame.size.height / 2;
+        _selectionIndicator.userInteractionEnabled = NO;
         [self addSubview:_selectionIndicator];
 
         // BOTÓN CHATS
@@ -44,44 +48,48 @@
         _profileButton.frame = CGRectMake(frame.size.width/2, 0, frame.size.width/2, frame.size.height);
         [_profileButton setTitle:@"Perfil" forState:UIControlStateNormal];
         
-        // MENÚ DEL PERFIL (Ver Estado y Ajustes)
         if (@available(iOS 14.0, *)) {
-            UIAction *verEstado = [UIAction actionWithTitle:@"Ver Estados" image:[UIImage systemImageNamed:@"circle.dashed"] identifier:nil handler:^(__kindof UIAction *action) {
+            UIAction *verEstado = [UIAction actionWithTitle:@"Ver Estados" image:nil identifier:nil handler:^(__kindof UIAction *action) {
                 [self goToUpdates];
             }];
-            UIAction *ajustes = [UIAction actionWithTitle:@"Ajustes" image:[UIImage systemImageNamed:@"gear"] identifier:nil handler:^(__kindof UIAction *action) {
+            UIAction *ajustes = [UIAction actionWithTitle:@"Ajustes" image:nil identifier:nil handler:^(__kindof UIAction *action) {
                 [self goToSettings];
             }];
-            
             _profileButton.menu = [UIMenu menuWithTitle:@"" children:@[verEstado, ajustes]];
             _profileButton.showsMenuAsPrimaryAction = YES;
+        } else {
+            // Si es iOS viejo, al menos que cambie de pestaña al tocar
+            [_profileButton addTarget:self action:@selector(goToSettings) forControlEvents:UIControlEventTouchUpInside];
         }
         [self addSubview:_profileButton];
     }
     return self;
 }
 
-// --- ACCIONES ---
+// --- ACCIONES MEJORADAS ---
 
 - (void)goToChats {
     [UIView animateWithDuration:0.2 animations:^{
         self.selectionIndicator.frame = CGRectMake(5, 5, (self.frame.size.width/2)-10, self.frame.size.height-10);
     }];
-    // En WhatsApp, la pestaña 3 suele ser Chats (o la 0 según versión)
-    if (_parentController) _parentController.selectedIndex = 3; 
+    if (self.parentController) {
+        [self.parentController setSelectedIndex:3]; // Pestaña Chats
+    }
 }
 
 - (void)goToUpdates {
-    [UIView animateWithDuration:0.2 animations:^{
-        self.selectionIndicator.frame = CGRectMake((self.frame.size.width/2)+5, 5, (self.frame.size.width/2)-10, self.frame.size.height-10);
-    }];
-    // La pestaña de "Novedades" (Estados)
-    if (_parentController) _parentController.selectedIndex = 0;
+    if (self.parentController) {
+        [self.parentController setSelectedIndex:0]; // Pestaña Novedades/Estados
+    }
 }
 
 - (void)goToSettings {
-    // La pestaña de Ajustes (Tú/Perfil)
-    if (_parentController) _parentController.selectedIndex = 4;
+    [UIView animateWithDuration:0.2 animations:^{
+        self.selectionIndicator.frame = CGRectMake((self.frame.size.width/2)+5, 5, (self.frame.size.width/2)-10, self.frame.size.height-10);
+    }];
+    if (self.parentController) {
+        [self.parentController setSelectedIndex:4]; // Pestaña Tú/Configuración
+    }
 }
 
 @end
@@ -92,33 +100,4 @@ static void (*orig_viewDidLayout)(UIViewController *, SEL);
 void hooked_viewDidLayout(UIViewController *self, SEL _cmd) {
     orig_viewDidLayout(self, _cmd);
 
-    if (![NSStringFromClass([self class]) isEqualToString:@"WATabBarController"]) return;
-
-    UITabBarController *tabC = (UITabBarController *)self;
-    tabC.tabBar.hidden = YES; // Oculta la original
-
-    UIWindow *keyWindow = [UIApplication sharedApplication].keyWindow;
-    if (!keyWindow) return;
-
-    MyCustomBar *myBar = (MyCustomBar *)[keyWindow viewWithTag:888];
-    if (!myBar) {
-        myBar = [[MyCustomBar alloc] initWithFrame:CGRectMake((keyWindow.frame.size.width - 280)/2, keyWindow.frame.size.height - 100, 280, 65) parent:tabC];
-        myBar.tag = 888;
-        [keyWindow addSubview:myBar];
-    }
-
-    // Ocultar si entramos a un chat o cámara
-    BOOL isInMain = (tabC.presentedViewController == nil && tabC.navigationController.viewControllers.count <= 1);
-    myBar.hidden = !isInMain;
-    if (isInMain) [keyWindow bringSubviewToFront:myBar];
-}
-
-__attribute__((constructor))
-static void init() {
-    Class targetClass = objc_getClass("WATabBarController");
-    if (targetClass) {
-        Method m = class_getInstanceMethod(targetClass, @selector(viewDidLayoutSubviews));
-        orig_viewDidLayout = (void *)method_getImplementation(m);
-        method_setImplementation(m, (IMP)hooked_viewDidLayout);
-    }
-}
+    if (![NSStringFromClass([self class]) isEqualToString
